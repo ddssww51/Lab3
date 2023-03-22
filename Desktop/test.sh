dfb2a08aba42171e27707c15210ff0a501758952
Mikołaj <pasek.mikolaj26@outlook.com>
2023-03-23 00:05:33 +0100

    testv2

--- Desktop/test.sh
+++ Desktop/test.sh
@@ -1,4 +1,11 @@
-test
-test
-test
-
+git commit
+git branch
+git checkout / git switch
+git merge
+git rebase
+git branch -f main HEAD~3
+git reset
+git revert
+git cherry-pick
+git rebase -i
+git clone
